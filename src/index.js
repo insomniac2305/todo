50fad4fe1d61a9c43d82bc9ba88c4359d09c354f
insomniac2305 <p.doerner@outlook.com>
2023-02-03 16:39:19 +0100

    Add grid layout and placeholder logo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import "./style.css";
+import Logo from "./assets/logo.svg";
 import Project from "./project";
 import ToDo from "./todo";
+
+const imgLogo = document.getElementById("logo");
+imgLogo.src = Logo;
 
 const project = Project("Default", "icon.png");
 const myToDo = ToDo("Laundry", "White and color", new Date("2023-02-24"), 1);
