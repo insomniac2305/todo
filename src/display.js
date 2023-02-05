0a8ab174fb693aac55e667f030af25dd36e408c8
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-02-05 16:40:26 +0100

    Add project and todo display methods

---
+++ src/display.js
@@ -0,0 +1,95 @@
+export default (() => {
+  const projectsNode = document.querySelector(".projects");
+  const newProjectNode = document.querySelector(".new-project");
+  // const projectTitleHeaderNode = document.querySelector(".header>.project-title");
+  const todoListNode = document.querySelector(".todo-list");
+  const newTodoNode = document.querySelector(".new-todo");
+  // const editTodoFormNode = document.querySelector("#edit-todo");
+  // const editTodoTitleNode = document.querySelector("#title");
+  // const editTodoDueDateNode = document.querySelector("#due");
+  // const selectTodoPrioNode = document.querySelector("#prio");
+  // const editTodoDescNode = document.querySelector("#desc");
+
+  const PRIORITIES = {
+    low: {
+      class: "prio-low",
+      icon: "keyboard_double_arrow_down",
+    },
+    med: {
+      class: "prio-med",
+      icon: "drag-handle",
+    },
+    high: {
+      class: "prio-high",
+      icon: "keyboard_double_arrow_up",
+    },
+  };
+
+  const DATEOPTIONS = {
+    year: "2-digit",
+    month: "2-digit",
+    day: "2-digit",
+  };
+
+  const createElemWithParam = (tag, className, innerText = "") => {
+    const element = document.createElement(tag);
+    element.classList.add(className);
+    element.innerText = innerText;
+    return element;
+  };
+
+  const addProjectDisplay = (projectTitle, projectIcon, projectColor, todoCount) => {
+    const projectNode = createElemWithParam("div", "project");
+    const projectIconWrap = createElemWithParam("div", "project-icon");
+    projectIconWrap.style.backgroundColor = projectColor;
+    const projectIconNode = createElemWithParam("span", "material-symbols-rounded", projectIcon);
+    projectIconWrap.appendChild(projectIconNode);
+    projectNode.appendChild(projectIconWrap);
+
+    const projectTitleNode = createElemWithParam("div", "project-title", projectTitle);
+    projectNode.appendChild(projectTitleNode);
+
+    const todoCountNode = createElemWithParam("div", "todo-count", todoCount);
+    projectNode.appendChild(todoCountNode);
+
+    projectsNode.insertBefore(projectNode, newProjectNode);
+  };
+
+  const addTodoDisplay = (todoTitle, dueDate, priority, done) => {
+    const todoNode = createElemWithParam("div", "todo");
+    done && todoNode.classList.add("done");
+    const todoDoneBtn = createElemWithParam("button", "todo-done-btn");
+    const todoDoneIcon = createElemWithParam("span", "material-symbols-rounded");
+    todoDoneIcon.classList.add("no-fill");
+    todoDoneBtn.appendChild(todoDoneIcon);
+    todoNode.appendChild(todoDoneBtn);
+
+    const todoTitleNode = createElemWithParam("div", "todo-title", todoTitle);
+    todoNode.appendChild(todoTitleNode);
+
+    const todoDateWrap = createElemWithParam("div", "todo-date");
+    const todoDateNode = createElemWithParam("div", "date", dueDate.toLocaleDateString(undefined, DATEOPTIONS));
+    todoDateWrap.appendChild(todoDateNode);
+    todoNode.appendChild(todoDateWrap);
+
+    const todoPrioWrap = createElemWithParam("div", "todo-priority");
+    const todoPrioIconNode = createElemWithParam("span", "material-symbols-rounded", PRIORITIES[priority].icon);
+    todoPrioIconNode.classList.add(PRIORITIES[priority].class);
+    todoPrioWrap.appendChild(todoPrioIconNode);
+    todoNode.appendChild(todoPrioWrap);
+
+    const todoEditBtn = createElemWithParam("button", "todo-edit-btn");
+    const todoEditIconNode = createElemWithParam("span", "material-symbols-rounded", "edit");
+    todoEditBtn.appendChild(todoEditIconNode);
+    todoNode.appendChild(todoEditBtn);
+
+    const todoDeleteBtn = createElemWithParam("button", "todo-delete-btn");
+    const todoDeleteIconNode = createElemWithParam("span", "material-symbols-rounded", "delete");
+    todoDeleteBtn.appendChild(todoDeleteIconNode);
+    todoNode.appendChild(todoDeleteBtn);
+
+    todoListNode.insertBefore(todoNode, newTodoNode);
+  };
+
+  return { addProjectDisplay, addTodoDisplay };
+})();
